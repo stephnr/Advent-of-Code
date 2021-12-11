9c484c3620454698a472326a0cb90bdfd5f3cd89
Stephen Rodriguez <steprodriguez10@gmail.com>
2021-12-11 13:51:21 +0100

    2021 - Day 10 - Part 2

--- 2021/10/main.go
+++ 2021/10/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"math"
+	"sort"
+	"strings"
 )
 
-func part_one(lines []string, part_two bool) (score int) {
-	starts := make([]rune, 0)
-	mismatches := make([]rune, 0)
+var (
+	leftAssignScorecard = "([{<"
+)
+
+func part_one(lines []string, part_two bool) (result uint64) {
+	scores := make([]uint64, 0)
 
 	for _, line := range lines {
+		var mismatch rune
+
+		starts := make([]rune, 0)
+		score := uint64(0)
+
 	CHAR_LOOP:
 		for _, char := range line {
 			switch char {
@@ -24,12 +34,12 @@ func part_one(lines []string, part_two bool) (score int) {
 			// Closer found
 			default:
 				if len(starts) == 0 {
-					mismatches = append(mismatches, char)
+					mismatch = char
 					break CHAR_LOOP
 				}
 
 				if math.Abs(float64(char-starts[len(starts)-1])) > 2 {
-					mismatches = append(mismatches, char)
+					mismatch = char
 					break CHAR_LOOP
 				} else {
 					if len(starts) >= 2 {
@@ -40,27 +50,46 @@ func part_one(lines []string, part_two bool) (score int) {
 				}
 			}
 		}
-	}
 
-	// Count score
-	for _, mismatch := range mismatches {
-		switch mismatch {
-		case ')':
-			score += 3
-		case ']':
-			score += 57
-		case '}':
-			score += 1197
-		case '>':
-			score += 25137
+		// Part Two
+		if part_two && len(starts) > 0 && mismatch == 0 {
+			// Score the missing incomplete parts
+			for i := len(starts) - 1; i >= 0; i-- {
+				// println(string(starts[i]), score)
+				// println(int(strings.IndexRune(leftAssignScorecard, starts[i])) + 1)
+				score = uint64(score*5 + (uint64(strings.IndexRune(leftAssignScorecard, starts[i])) + 1))
+			}
+
+			scores = append(scores, score)
+		} else {
+			// Part One - score mismatches
+			switch mismatch {
+			case ')':
+				score += 3
+			case ']':
+				score += 57
+			case '}':
+				score += 1197
+			case '>':
+				score += 25137
+			}
+
+			result += score
 		}
 	}
 
-	return score
+	if part_two {
+		// Find median of scores
+		sort.Slice(scores, func(i, j int) bool { return scores[i] < scores[j] })
+		return scores[len(scores)/2]
+	}
+
+	// Part One: Just return the sum of mismatches
+	return result
 }
 
 func main() {
 	lines := ReadFile("./inputs/1")
-	println(part_one(lines))
-	// println(part_two())
+	println(part_one(lines, false))
+	println(part_one(lines, true))
 }
--- 2021/10/main_test.go
+++ 2021/10/main_test.go
@@ -3,13 +3,17 @@ package main
 import "testing"
 
 func BenchmarkPartOne(b *testing.B) {
+	lines := ReadFile("./inputs/1")
+
 	for n := 0; n <= b.N; n++ {
-		part_one()
+		part_one(lines, false)
 	}
 }
 
 func BenchmarkPartTwo(b *testing.B) {
+	lines := ReadFile("./inputs/1")
+
 	for n := 0; n <= b.N; n++ {
-		// part_two()
+		part_one(lines, true)
 	}
 }
