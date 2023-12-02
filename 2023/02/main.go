3aec87ac0484934144ee1fc3deb4aff00f5e45ea
Stephen Rodriguez <steprodriguez10@gmail.com>
2023-12-02 23:44:19 +0100

    2023/02

---
+++ 2023/02/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	_ "embed"
+	"math"
+	"regexp"
+	"strconv"
+)
+
+//go:embed input.txt
+var input string
+
+var rowIdxRegex = regexp.MustCompile(`Game\s(\d+)`)
+var gameSplitRegex = regexp.MustCompile(`((\d{1,2})\s(r|g|b))`)
+
+func partOne(rows []string) int {
+	var result int
+
+	max := map[string]int{"r": 12, "g": 13, "b": 14}
+
+RowStep:
+	for _, row := range rows {
+		rowID, _ := strconv.Atoi(rowIdxRegex.FindStringSubmatch(row)[1])
+		gameResults := gameSplitRegex.FindAllStringSubmatch(row, -1)
+
+		for _, cubeCaptures := range gameResults {
+			cubeCount, _ := strconv.Atoi(cubeCaptures[2])
+			if cubeCount > max[cubeCaptures[3]] {
+				// Invalid game state - skip
+				continue RowStep
+			}
+		}
+
+		result += rowID
+	}
+
+	return result
+}
+
+func partTwo(rows []string) float64 {
+	var result float64
+
+	for _, row := range rows {
+		gameResults := gameSplitRegex.FindAllStringSubmatch(row, -1)
+		maxPowerSet := map[string]float64{"r": 0, "g": 0, "b": 0}
+
+		for _, cubeCaptures := range gameResults {
+			cubeCount, _ := strconv.Atoi(cubeCaptures[2])
+			maxPowerSet[cubeCaptures[3]] = math.Max(float64(cubeCount), float64(maxPowerSet[cubeCaptures[3]]))
+		}
+
+		result += (maxPowerSet["r"] * maxPowerSet["g"] * maxPowerSet["b"])
+	}
+
+	return result
+}
+
+func main() {
+	println(partOne(parseRows(input)))
+	println(strconv.Itoa(int(partTwo(parseRows(input)))))
+}
