10f395d313cae81225ad2ee863cfc24f19bc8c99
Stephen Rodriguez <steprodriguez10@gmail.com>
2023-12-04 19:45:42 +0100

    2023/04 - cleanup

--- 2023/04/solution.py
+++ 2023/04/solution.py
@@ -4,16 +4,18 @@ def load(file):
 
 
 def solve(p):
+  # Form result trackers & scratch tables to track copies
   part1, cards = 0, [1] * len(p)
+
   for id, row in enumerate(p):
     # Count the number of winners by reading the length of an intersection
     if winner := len(set.intersection(*[set(map(int,part.split())) for part in row.split('|')])):
       # Part 1 => counting the score exponentially on base 2
       part1 += 2**(winner-1)
-      # Part 2 => Form a dynamic range and count the number of cards formed without nesting
+      # Part 2 => Count the number of cards formed without nesting by ranging over
       for n in range(1, winner+1):
         cards[id+n] += cards[id]
   return part1, sum(cards)
 
 
-print(f'Part 1: {solve(load("input.txt"))}')
+print(f'Solutions: {solve(load("input.txt"))}')
