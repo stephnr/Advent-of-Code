50beec438469900ecb9f6a742f3950c2f58abec3
Stephen Rodriguez <steprodriguez10@gmail.com>
2023-12-06 20:52:23 +0100

    solved 2023/06 - part 2

--- 2023/06/solution.py
+++ 2023/06/solution.py
@@ -4,16 +4,26 @@ def load(file):
   with open(file) as f:
     return [row.strip() for row in f]
 
-def solve(p) -> int:
-  times     = [int(el) for el in p[0].split(':')[1].strip().split(' ') if len(el) > 0]
-  distances = [int(el) for el in p[1].split(':')[1].strip().split(' ') if len(el) > 0]
-  winPaths  = [0] * len(times)
+def solve(p):
+  times  = [int(el) for el in p[0].split(':')[1].strip().split(' ') if len(el) > 0]
+  dists  = [int(el) for el in p[1].split(':')[1].strip().split(' ') if len(el) > 0]
+  p2time = int(reduce(lambda x, y: ''+str(x)+str(y), times))
+  p2dist = int(reduce(lambda x, y: ''+str(x)+str(y), dists))
 
+  winPaths, p2winPath  = [0] * len(times), 0
+
+  # Solve Part 1
   for i in range(0, len(times)):
     for speed in range(1, times[i]):
-      if speed * (times[i] - speed) > distances[i]:
+      if speed * (times[i] - speed) > dists[i]:
         winPaths[i] += 1
 
-  return reduce(lambda x, y: x*y, winPaths)
+  # Solve Part 2
+  for speed in range(1, p2time):
+    if speed * (p2time - speed) > p2dist:
+      p2winPath += 1
 
-print(f'Solution: ({solve(load("input.txt"))})')
+  return reduce(lambda x, y: x*y, winPaths), p2winPath
+
+p = load("input.txt")
+print(f'Solution: {solve(p)}')
