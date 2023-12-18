f5ba75d6f845ff90748793b701ae1039005479c8
Stephen Rodriguez <steprodriguez10@gmail.com>
2023-12-18 21:12:47 +0100

    finish 2023/08 - part 2

--- 2023/08/solution.py
+++ 2023/08/solution.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from math import gcd
 import re
 
 def load(file):
@@ -12,6 +14,9 @@ def loadNodes(lines):
     nodes[node] = tuple([left, right])
 
   return nodes
+
+def lcm(a, b):
+  return a * b // gcd(a, b)
 
 def part1(p):
   steps = list(p[0])
@@ -41,7 +46,40 @@ def part1(p):
   return count
 
 def part2(p):
-  return 0
+  steps = list(p[0])
+  nodes = loadNodes(p)
+
+  # Start traversal
+  node, count = [node for node in nodes if node[-1] == "A"], 0
+  steps_needed = []
+
+  # Find steps needed to reach each of the Z nodes
+  while len(node) > 0:
+    step = steps[count % len(steps)]
+    count += 1
+
+    newNodes = []
+
+    for n in node:
+      match step:
+        case 'L':
+          if n[-1] == 'Z':
+            steps_needed.append(count-1)
+          else:
+            newNodes.append(nodes[n][0])
+
+          pass
+        case 'R':
+          if n[-1] == 'Z':
+            steps_needed.append(count-1)
+          else:
+            newNodes.append(nodes[n][1])
+
+          pass
+
+    node = newNodes
+
+  return reduce(lcm, steps_needed)
 
 p = load("input.txt")
 print(f'Solution: {part1(p), part2(p)}')
