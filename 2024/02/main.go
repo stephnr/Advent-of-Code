db40faef5f2d75bad1a934e5c7065a7877e989f7
Stephen Rodriguez <steprodriguez10@gmail.com>
2024-12-02 10:00:20 +0000

    2024: 02 P2 complete

--- 2024/02/main.go
+++ 2024/02/main.go
@@ -5,15 +5,22 @@ import (
 	"strings"
 )
 
-func countSafeReports(filepath string) int {
+func countSafeReports(filepath string, dampener bool) int {
 	lines := ReadInput(filepath)
 	sum := 0
 
 	for line := range lines {
-		inc := isInc(line)
-		dec := isDec(line)
+		l := strings.Split(line, " ")
+		inc := isInc(l)
+		dec := isDec(l)
 
 		if inc || dec {
+			sum += 1
+			continue
+		}
+
+		// Check with slice iterations
+		if dampener && checkWithDampener(line) {
 			sum += 1
 		}
 	}
@@ -21,9 +28,32 @@ func countSafeReports(filepath string) int {
 	return sum
 }
 
-func isDec(ss string) bool {
+func checkWithDampener(ss string) bool {
 	s := strings.Split(ss, " ")
 
+	for i := 0; i < len(s); i++ {
+		if i == 0 {
+			if isInc(s[1:]) || isDec(s[1:]) {
+				return true
+			}
+		} else if i == len(s) {
+			if isInc(s[:len(s)-1]) || isDec(s[:len(s)-1]) {
+				return true
+			}
+		} else {
+			arr := []string{}
+			arr = append(arr, s[0:i]...)
+			arr = append(arr, s[i+1:]...)
+			if isInc(arr) || isDec(arr) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func isDec(s []string) bool {
 	for i := 1; i < len(s); i++ {
 		l, _ := strconv.Atoi(s[i-1])
 		r, _ := strconv.Atoi(s[i])
@@ -36,9 +66,7 @@ func isDec(ss string) bool {
 	return true
 }
 
-func isInc(ss string) bool {
-	s := strings.Split(ss, " ")
-
+func isInc(s []string) bool {
 	for i := 1; i < len(s); i++ {
 		l, _ := strconv.Atoi(s[i-1])
 		r, _ := strconv.Atoi(s[i])
@@ -52,5 +80,6 @@ func isInc(ss string) bool {
 }
 
 func main() {
-	println(countSafeReports("./inputs/1.txt"))
+	println(countSafeReports("./inputs/1.txt", false))
+	println(countSafeReports("./inputs/1.txt", true))
 }
