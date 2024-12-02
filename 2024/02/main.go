c507298c79eb094a14aa6ad68a6ddce9ccf4edee
Stephen Rodriguez <steprodriguez10@gmail.com>
2024-12-02 08:46:36 +0000

    2024: 02 P1 complete

--- 2024/02/main.go
+++ 2024/02/main.go
@@ -1,16 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"strconv"
+	"strings"
+)
 
-func run(filepath string) int {
+func countSafeReports(filepath string) int {
 	lines := ReadInput(filepath)
 	sum := 0
 
-	fmt.Printf("%+v\n", lines)
+	for line := range lines {
+		inc := isInc(line)
+		dec := isDec(line)
+
+		if inc || dec {
+			sum += 1
+		}
+	}
 
 	return sum
 }
 
+func isDec(ss string) bool {
+	s := strings.Split(ss, " ")
+
+	for i := 1; i < len(s); i++ {
+		l, _ := strconv.Atoi(s[i-1])
+		r, _ := strconv.Atoi(s[i])
+		diff := (r - l)
+		if diff < 1 || diff > 3 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isInc(ss string) bool {
+	s := strings.Split(ss, " ")
+
+	for i := 1; i < len(s); i++ {
+		l, _ := strconv.Atoi(s[i-1])
+		r, _ := strconv.Atoi(s[i])
+		diff := (l - r)
+		if diff < 1 || diff > 3 {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main() {
-	println(run("./inputs/1.txt"))
+	println(countSafeReports("./inputs/1.txt"))
 }
