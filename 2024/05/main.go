0f0562751d92166ea04e9941c50ce2674e742d82
Stephen Rodriguez <steprodriguez10@gmail.com>
2024-12-05 12:22:36 +0000

    2024: 05 P1 complete

--- 2024/05/main.go
+++ 2024/05/main.go
@@ -1,12 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 func run(filepath string) int {
 	lines := ReadInput(filepath)
+	rules := map[int][]int{}
 	sum := 0
 
-	fmt.Printf("%+v\n", lines)
+Check:
+	for line := range lines {
+		if strings.Contains(line, "|") {
+			ss := strings.Split(line, "|")
+			a, _ := strconv.Atoi(ss[0])
+			b, _ := strconv.Atoi(ss[1])
+			rules[a] = append(rules[a], b)
+		}
+
+		// Line check
+		if strings.Contains(line, ",") {
+			ss := strings.Split(line, ",")
+
+			for i := 1; i < len(ss); i++ {
+				p := ss[i]
+				pp, _ := strconv.Atoi(string(p))
+
+				for _, el := range ss[:i] {
+					ele, _ := strconv.Atoi(string(el))
+					if slices.Contains(rules[pp], ele) {
+						continue Check
+					}
+				}
+			}
+
+			el, _ := strconv.Atoi(ss[int(math.Ceil(float64(len(ss)/2)))])
+
+			sum += el
+		}
+	}
 
 	return sum
 }
