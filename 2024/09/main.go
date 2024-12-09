65309fd54e219b043889d00869f32c8d6d22c0c5
Stephen Rodriguez <steprodriguez10@gmail.com>
2024-12-09 12:54:19 +0100

    2024: 09 P1 complete

--- 2024/09/main.go
+++ 2024/09/main.go
@@ -1,14 +1,147 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type Disk struct {
+	ID     int
+	Length int
+	IsFree bool
+}
 
 func run(filepath string) int {
 	lines := ReadInput(filepath)
 	sum := 0
 
-	fmt.Printf("%+v\n", lines)
+	diskCount := -1
+	diskmap := []Disk{}
+
+	for line := range lines {
+		l := strings.Split(line, "")
+
+		for i := 0; i < len(l); i++ {
+			// File Block
+			if i%2 == 0 {
+				diskCount++
+				a, _ := strconv.Atoi(l[i])
+				diskmap = append(diskmap, Disk{ID: diskCount, Length: a, IsFree: false})
+			}
+
+			// Free-Space Block
+			if i%2 == 1 {
+				a, _ := strconv.Atoi(l[i])
+				diskmap = append(diskmap, Disk{Length: a, IsFree: true})
+			}
+		}
+	}
+
+	cont := false
+
+	for {
+		diskmap, cont = moveFileBlocks(diskmap)
+
+		if !cont {
+			break
+		}
+	}
+
+	// Calculate the checksum
+	diskString := getDiskMap(diskmap)
+	fmt.Printf("%+v\n", diskString)
+
+	id := 0
+	for _, disk := range diskmap {
+		if disk.IsFree {
+			break
+		}
+
+		for i := 0; i < disk.Length; i++ {
+			sum += (id * disk.ID)
+			id++
+		}
+	}
 
 	return sum
+}
+
+// Sends true if restart
+func moveFileBlocks(diskmap []Disk) ([]Disk, bool) {
+	// Squeeze from right side into left-most empty bits
+	for i, disk := range diskmap {
+		if !disk.IsFree {
+			continue
+		}
+
+		// Find right-most disk
+		rD := -1
+
+		for j := len(diskmap) - 1; j >= 0; j-- {
+			if !diskmap[j].IsFree && diskmap[j].Length > 0 {
+				rD = j
+				break
+			}
+		}
+
+		if rD <= i {
+			return diskmap, false
+		}
+
+		// Fill free space when space is equals
+		if disk.Length == diskmap[rD].Length {
+			diskmap[i].ID = diskmap[rD].ID
+			diskmap[i].IsFree = false
+			diskmap[rD].IsFree = true
+			return diskmap, true
+		} else if disk.Length < diskmap[rD].Length {
+			// Fill when free needs less than rightmost
+			diskmap[i].ID = diskmap[rD].ID
+			diskmap[i].IsFree = false
+
+			diskmap[rD].Length -= diskmap[i].Length
+
+			diskmap = slices.Insert(diskmap, rD+1, Disk{
+				ID:     -1,
+				Length: diskmap[i].Length,
+				IsFree: true,
+			})
+			return diskmap, true
+		} else if disk.Length > diskmap[rD].Length {
+			diskmap[i].Length -= diskmap[rD].Length
+			diskmap[rD].IsFree = true
+
+			// Fill when free space is larger than rightmost
+			diskmap = slices.Insert(diskmap, i, Disk{
+				ID:     diskmap[rD].ID,
+				Length: diskmap[rD].Length,
+				IsFree: false,
+			})
+
+			return diskmap, true
+		}
+	}
+
+	return diskmap, false
+}
+
+func getDiskMap(diskmap []Disk) string {
+	var s bytes.Buffer
+
+	for _, disk := range diskmap {
+		for j := 0; j < disk.Length; j++ {
+			if disk.IsFree {
+				s.WriteString(".")
+			} else {
+				s.WriteString(strconv.Itoa(disk.ID))
+			}
+		}
+	}
+
+	return s.String()
 }
 
 func main() {
---
+++ 2024/10/utils.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func ReadFile(filepath string) []string {
+	lines := make([]string, 0)
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lines
+}
+
+func ReadInput(filepath string) <-chan string {
+	lines := ReadFile(filepath)
+
+	// Convert it to a channel
+	out := make(chan string, len(lines))
+	defer close(out)
+
+	for _, line := range lines {
+		out <- line
+	}
+
+	return out
+}
